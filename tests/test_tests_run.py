867feaadccea0338ad8f62c5c0b210dd867054f6
Aiky30 <aiky30@yahoo.com>
2018-09-10 11:53:44 +0100

    Amend typos

--- tests/test_tests_run.py
+++ tests/test_tests_run.py
@@ -1,8 +1,8 @@
 from cms.test_utils.testcases import CMSTestCase
 
 
-#FIXME: REMOVEME: This file just tests thta the test suite works.
-#   There are no testcases yet, when there are this is redundnat and should be deleted.
+# FIXME: REMOVEME: This file just tests that the test suite is configured and works.
+#   There are no testcases yet, when there are this file is redundant and should be deleted.
 class ExampleTestCase(CMSTestCase):
 
     def test_suite_works(self):

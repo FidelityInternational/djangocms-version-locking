4332a99d1342f800561ea0dc51d3152e37635c58
Aiky30 <aiky30@yahoo.com>
2018-09-10 11:46:50 +0100

    Amended the test with a note to remove as well as including the missing settings import for the model

---
+++ tests/test_tests_run.py
@@ -0,0 +1,9 @@
+from cms.test_utils.testcases import CMSTestCase
+
+
+#FIXME: REMOVEME: This file just tests thta the test suite works.
+#   There are no testcases yet, when there are this is redundnat and should be deleted.
+class ExampleTestCase(CMSTestCase):
+
+    def test_suite_works(self):
+        self.assertTrue(True)

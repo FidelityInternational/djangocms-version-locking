a59ef74b4f376ac0239335f8276903fe4f833bf4
Krzysztof Socha <k.socha@merixstudio.com>
2018-09-21 13:33:59 +0200

    Rename has_change_permission argument to obj

--- djangocms_version_locking/admin.py
+++ djangocms_version_locking/admin.py
@@ -1,5 +1,4 @@
 from djangocms_versioning.admin import VersioningAdminMixin
-from djangocms_versioning.models import Version
 
 
 class VersionLockAdminMixin(VersioningAdminMixin):
@@ -8,7 +7,7 @@ class VersionLockAdminMixin(VersioningAdminMixin):
     version models.
     """
 
-    def has_change_permission(self, request, content_obj=None):
+    def has_change_permission(self, request, obj=None):
         """
         If there’s a lock for edited object and if that lock belongs
         to the current user
@@ -16,12 +15,12 @@ class VersionLockAdminMixin(VersioningAdminMixin):
         from .helpers import content_is_unlocked
 
         # User has permissions?
-        has_permission = super().has_change_permission(request, content_obj)
+        has_permission = super().has_change_permission(request, obj)
         if not has_permission:
             return False
 
         # Check if the lock exists and belongs to the user
-        if content_obj:
-            return content_is_unlocked(content_obj, request.user)
+        if obj:
+            return content_is_unlocked(obj, request.user)
 
         return True

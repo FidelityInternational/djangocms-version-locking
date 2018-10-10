17daf6a0534e11fdf73eae1220a1570b431a929d
Aiky30 <aiky30@yahoo.com>
2018-10-10 14:45:07 +0100

    Finished populating the email template and now tests that the email notification is sen twhen it should be

--- djangocms_version_locking/emails.py
+++ djangocms_version_locking/emails.py
@@ -1,13 +1,14 @@
 from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
 
 from cms.utils import get_current_site
 
 from .helpers import send_email
+from .utils import get_absolute_url
 
 
 def notify_version_author_version_unlocked(version, unlocking_user):
-
-    # If the unlocking user is the current author, don't send an email
+    # If the unlocking user is the current author, don't send a notification email
     if version.created_by == unlocking_user:
         return
 
@@ -18,18 +19,22 @@ def notify_version_author_version_unlocked(version, unlocking_user):
         page_title=version.content,
         description=_("Unlocked"),
     )
+    version_url = reverse(
+        'admin:cms_placeholder_render_object_preview',
+        args=(version.content_type_id, version.object_id),
+    )
+    version_url = get_absolute_url(version_url)
 
+    # Prepare and send the email
     template_context = {
         'author_name': version.created_by,
-        'page_url': '',
+        'version_link': version_url,
         'by_user': unlocking_user,
     }
-
     status = send_email(
         recipients=recipients,
         subject=subject,
         template='unlock-notification.txt',
         template_context=template_context,
     )
-
     return status
---
+++ tests/test_emails.py
@@ -0,0 +1,81 @@
+from django.contrib.auth.models import Permission
+from django.core import mail
+from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
+
+from cms.test_utils.testcases import CMSTestCase
+from cms.utils import get_current_site
+
+from djangocms_versioning.cms_config import VersioningCMSConfig
+from djangocms_versioning.test_utils import factories
+
+from djangocms_version_locking.utils import get_absolute_url
+
+
+class VersionLockNotificationEmailsTestCase(CMSTestCase):
+
+    def setUp(self):
+        self.superuser = self.get_superuser()
+        self.user_author = self._create_user("author", is_staff=True, is_superuser=False)
+        self.user_has_no_perms = self._create_user("user_has_no_perms", is_staff=True, is_superuser=False)
+        self.user_has_unlock_perms = self._create_user("user_has_unlock_perms", is_staff=True, is_superuser=False)
+        self.versionable = VersioningCMSConfig.versioning[0]
+
+        # Set permissions
+        delete_permission = Permission.objects.get(codename='delete_versionlock')
+        self.user_has_unlock_perms.user_permissions.add(delete_permission)
+
+    def test_notify_version_author_version_unlocked_email_sent_for_different_user(self):
+        """
+        The user unlocking a version that is authored buy a different user
+        should be sent a notification email
+        """
+        draft_version = factories.PageVersionFactory(content__template="", created_by=self.user_author)
+        draft_unlock_url = self.get_admin_url(self.versionable.version_model_proxy,
+                                              'unlock', draft_version.pk)
+
+        # Check that no emails exist
+        self.assertEqual(len(mail.outbox), 0)
+
+        # Unlock the version with a different user with unlock permissions
+        with self.login_user_context(self.user_has_unlock_perms):
+            self.client.post(draft_unlock_url, follow=True)
+
+        site = get_current_site()
+        expected_subject = "[Django CMS] ({site_name}) {page_title} - {description}".format(
+            site_name=site.name,
+            page_title=draft_version.content,
+            description=_("Unlocked"),
+        )
+        expected_body = "The following draft version has been unlocked by {by_user} for their use.".format(
+            by_user=self.user_has_unlock_perms
+        )
+        expected_version_url = reverse(
+            'admin:cms_placeholder_render_object_preview',
+            args=(draft_version.content_type_id, draft_version.object_id),
+        )
+        expected_version_url = get_absolute_url(expected_version_url)
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, expected_subject)
+        self.assertTrue(expected_body in mail.outbox[0].body)
+        self.assertTrue(expected_version_url in mail.outbox[0].body)
+
+    def test_notify_version_author_version_unlocked_email_not_sent_for_different_user(self):
+        """
+        The user unlocking a version that authored the version should not be
+        sent a notification email
+        """
+        draft_version = factories.PageVersionFactory(content__template="", created_by=self.user_author)
+        draft_unlock_url = self.get_admin_url(self.versionable.version_model_proxy,
+                                              'unlock', draft_version.pk)
+
+        # Check that no emails exist
+        self.assertEqual(len(mail.outbox), 0)
+
+        # Unlock the version the same user who authored it
+        with self.login_user_context(self.user_author):
+            self.client.post(draft_unlock_url, follow=True)
+
+        # Check that no emails still exist
+        self.assertEqual(len(mail.outbox), 0)

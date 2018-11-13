df67d000807ada0b4c086ad81b9201182da51732
Aiky30 <aiky30@yahoo.com>
2018-11-13 16:45:54 +0000

    Feedback from PR: amended the method of querying for a version to support versioning filer. Cleaned button template file spacing

--- djangocms_version_locking/helpers.py
+++ djangocms_version_locking/helpers.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.encoding import force_text
+
+from djangocms_versioning.models import Version
 
 from .admin import VersionLockAdminMixin
 from .models import VersionLock
@@ -55,11 +57,9 @@ def get_lock_for_content(content):
     """Check if a lock exists, if so return it
     """
     try:
-        return VersionLock.objects.get(
-            version__content_type=ContentType.objects.get_for_model(content),
-            version__object_id=content.pk,
-        )
-    except VersionLock.DoesNotExist:
+        version = Version.objects.select_related('versionlock').get_for_content(content)
+        return version.versionlock
+    except ObjectDoesNotExist:
         return None
 
 
@@ -67,11 +67,9 @@ def content_is_unlocked_for_user(content, user):
     """Check if lock doesn't exist or object is locked to provided user.
     """
     try:
-        lock = VersionLock.objects.get(
-            version__content_type=ContentType.objects.get_for_model(content),
-            version__object_id=content.pk,
-        )
-    except VersionLock.DoesNotExist:
+        version = Version.objects.select_related('versionlock').get_for_content(content)
+        lock = version.versionlock
+    except ObjectDoesNotExist:
         return True
     return lock.created_by == user
 

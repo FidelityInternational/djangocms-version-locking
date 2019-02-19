cea14e41ee7331f3f417745d794be91827d59c78
Rizwan Mansuri <Rizwan@webbyfox.com>
2019-02-19 09:42:15 +0000

    Refactoring

--- djangocms_version_locking/apps.py
+++ djangocms_version_locking/apps.py
@@ -3,8 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class VersionLockingConfig(AppConfig):
-    name = 'djangocms_version_locking'
-    verbose_name = _('django CMS Version Locking')
+    name = "djangocms_version_locking"
+    verbose_name = _("django CMS Version Locking")
 
     def ready(self):
         from .monkeypatch import checks, cms_toolbars, models
+        from .monkeypatch import admin as monkeypatch_admin
